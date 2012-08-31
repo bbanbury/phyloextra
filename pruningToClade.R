fbae152f80cd242de7d0f03926e4cf0ff35f119e
bbanbury <darwinthesun@gmail.com>
2012-08-31 18:16:46 -0400

    removing .R from main branch

--- pruningToClade.R
+++
@@ -1,9 +0,0 @@
-pruningToClade<-function(phy, node) {
-#author: B. Banbury
-#function for pruning a tree to a single clade
-	g<-as.matrix(c(node.leaves(phy,node)))
-	rownames(g)<-g[,1]
-	d<-name.check(phy, g)
-	phy2<-drop.tip(phy, d[[1]])
-	return(phy2)
-}
