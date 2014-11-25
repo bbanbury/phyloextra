89f3492b22dd944c8cb0602273002f230d142b63
Barb Banbury <darwinthesun@gmail.com>
2014-11-25 11:25:35 -0800

    reoxygenized

---
+++ R/phyloextra.R
@@ -0,0 +1,17 @@
+#' phyloextra - Set of tools that I have found useful over the years
+#'
+#' A set of totally random functions that I have made over the years to help me code.  There is no rhyme or reason and there may be better fnctions out there that do the same thing.  
+#' 
+#' \tabular{ll}{
+#' Package: \tab phyloextra \cr
+#' Type: \tab Package \cr
+#' Version: \tab 1.0 \cr
+#' Date: \tab 2014-10-25 \cr
+#' License: \tab GPL(>= 2) \cr
+#' }
+#' @aliases phyloextra
+#' @author Barb Banbury 
+#' @docType package
+#' @name phyloextra
+#' @import geiger
+NULL
